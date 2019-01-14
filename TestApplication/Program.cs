8bc4f2eca12807d3b4988601fefcc99aaf008c16
samuel grahame <samuel@intelogy.com.au>
2019-01-15 09:21:54 +1100

    new demp

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TestApplication
+namespace Test
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            var x = new frmAnchorTest();
+            
+            Application.Run(x);
         }
     }
 }
--- TestApplication/frmModal.cs
+++ TestApplication/frmModal.cs
@@ -8,18 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TestApplication
+namespace Test
 {
-    public partial class Form1 : Form
+    public partial class frmModal : Form
     {
-        public Form1()
+        public frmModal()
         {
             InitializeComponent();
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"This button is anchored with: {(sender as Button)?.Text}");
+            this.Close();
         }
     }
 }
